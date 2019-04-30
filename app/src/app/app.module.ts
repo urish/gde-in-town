57190fa60bf35fe391f99bb3b06c6e50e2b7e88d
Uri Shaked <uri@urishaked.com>
2019-04-30 23:56:39 +0300

    feat(app): add firebase auth (twitter)

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [firebase.auth.TwitterAuthProvider.PROVIDER_ID],
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDcSqneOMAZ4-0XtpNZtPz7vHpxPTfl3JI',
+      authDomain: 'gdeintown.firebaseapp.com',
+      databaseURL: 'https://gdeintown.firebaseio.com',
+      projectId: 'gdeintown',
+      storageBucket: 'gdeintown.appspot.com',
+      messagingSenderId: '531225954547',
+    }),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
